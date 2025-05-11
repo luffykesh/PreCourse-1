e276df2955d119d020a48719038f079d5129f7da
Rushikesh Patel <the.rushikesh.patel@gmail.com>
2025-05-10 21:50:52 -0300

    exercise 2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,4 +1,6 @@
-
+# Space: O(1)
+# Push: O(1)
+# Pop: O(1)
 class Node:
     def __init__(self, data):
        self.data = data
@@ -6,16 +8,36 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.top = None
+        self.length = 0
     def push(self, data):
+        n = Node(data)
+        n.next = self.top
+        self.top = n
+        self.length += 1
         
     def pop(self):
+        if self.top is None:
+            return None
+        t = self.top.data
+        self.top = self.top.next
+        
+        return t
+    
+    def show(self):
+        head = self.top
+        while head is not None:
+            print(head.data, end='->')
+            head=head.next
+        print("NULL")
+        
         
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
     print('pop')
+    print('show list(s)')
     print('quit')
     do = input('What would you like to do? ').split()
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
@@ -28,5 +50,7 @@ while True:
             print('Stack is empty.')
         else:
             print('Popped value: ', int(popped))
+    elif operation == 's':
+        a_stack.show()
     elif operation == 'quit':
         break
